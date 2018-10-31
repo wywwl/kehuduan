0ff7aaceaf253c48f0609d7cc8bba351cc3eb5ce
Wuyy1 <1101820835@.qq.com>
2018-10-31 17:29:50 +0800

    最新版

--- src/main/java/com/jk/util/GetRabbitMqData.java
+++ src/main/java/com/jk/util/GetRabbitMqData.java
@@ -7,8 +7,16 @@ package com.jk.util;
 @org.springframework.stereotype.Component
 @org.springframework.amqp.rabbit.annotation.RabbitListener(queues = {"A1804"})
 public class GetRabbitMqData {
-    public GetRabbitMqData() { /* compiled code */ }
+
+    public GetRabbitMqData() {
+        /* compiled code */
+    }
 
     @org.springframework.amqp.rabbit.annotation.RabbitHandler
-    public void getMsg(String msg) { /* compiled code */ }
+    public void getMsg(String msg) {
+        /* compiled code */
+    }
+
+
+
 }
